30fd9daeab4d3015757505cff550e603f49dd2e1
subtly <subtly@users.noreply.github.com>
2019-04-30 02:32:54 +0800

    fix for electron w/dynamic loading of solcjson
    This is a fix so that remix will work inside of electron, and presumably, it makes sense for dynamic downloading of solidity compiler. When I enable node integration with electron and install solc node module, it looks like it works for a second, but then it too does not work (page goes blank).
    
    I'm not sure if require() works here with node, as I believe solcjson would need to be downloaded into a path that can be resolved by require (typically node_modules). Notably, electron docs recommend against enabling node integration when remote scripts are loaded dynamically.

--- remix-solidity/src/compiler/compiler.js
+++ remix-solidity/src/compiler/compiler.js
@@ -73,8 +73,8 @@ function Compiler (handleImportCall) {
   function onInternalCompilerLoaded () {
     if (worker === null) {
       var compiler
-      var userAgent = (typeof (navigator) !== 'undefined') && navigator.userAgent ? navigator.userAgent.toLowerCase() : '-'
-      if (typeof (window) === 'undefined' || userAgent.indexOf(' electron/') > -1) {
+      var userAgent = typeof navigator !== 'undefined' && navigator.userAgent ? navigator.userAgent.toLowerCase() : '-'
+      if (typeof window === 'undefined') {
         compiler = require('solc')
       } else {
         compiler = solc(window.Module)
